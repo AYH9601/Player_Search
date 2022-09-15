ee541eea66022a1c6513d6028972b5bed3aa911e
AYH9601 <ays9499@naver.com>
2022-09-15 17:35:29 +0900

    stage 2

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/css"; //basic
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+SwiperCore.use([Navigation, Pagination]);
 
 function Main() {
     return (
@@ -13,6 +20,8 @@ function Main() {
                 <ContentsBox></ContentsBox>
                 <ContentsBox></ContentsBox>
             </ContentsMain>
+
+            <ConTentsSub></ConTentsSub>
         </Contents>
     )
 };
@@ -46,6 +55,22 @@ function ContentsBox() {
             <p className="boxText"> LOGTEXT 1</p>
             <p className="boxDate">LOG DATE</p>
         </ContentsMainBox>
+    )
+};
+
+function ConTentsSub() {
+    return (
+        <ContentsSubDiv>
+            <h1>ARCHIVE</h1>
+            <div className="swiperSection">
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                </Swiper>
+            </div>
+        </ContentsSubDiv>
     )
 };
 
@@ -89,6 +114,7 @@ const ContentsHeaderDiv = styled.div`
 
 const ContentsMain = styled.div`
     width: 100%;
+    margin-bottom: 100px;
     &::after {content:""; display:block; clear:both;}
 `;
 
@@ -122,5 +148,24 @@ const ContentsMainBox = styled.div`
     .boxDate {
         text-align: left;
         color: #999;
+    }
+`;
+
+const ContentsSubDiv = styled.div`
+    padding: 90px;
+    h1 {
+        text-align: left;
+        font: bold 40px/1 "Lato";
+        margin-bottom: 30px;
+    }
+    .swiperSection {
+        .swiper {
+            width: 100%;
+            height: 500px;
+            background-color: crimson;
+            text-align: center;
+            font: bold 20px/500px "Lato";
+            color: #fff;
+        }
     }
 `;
